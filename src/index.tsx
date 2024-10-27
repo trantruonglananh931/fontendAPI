3bd2d4d6a464a565a17681a6f3b400f986fc0691
Lan Anh <trantruonglananh225@gmail.com>
2024-10-27 16:03:43 +0700

    luu fontend

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import { GoogleOAuthProvider } from "@react-oauth/google"; // Import GoogleOAuthProvider
+import { GoogleOAuthProvider } from "@react-oauth/google"; 
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { router } from "./Routes/Routes";
 
-// Tạo root để render ứng dụng
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-// Thêm GoogleOAuthProvider và truyền vào clientId
+
 root.render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId="439897666887-f83oja1bcobqc1nuju7h2phaofk4f8m6.apps.googleusercontent.com">
@@ -21,5 +21,5 @@ root.render(
   </React.StrictMode>
 );
 
-// Đo hiệu năng ứng dụng nếu cần
+
 reportWebVitals();
